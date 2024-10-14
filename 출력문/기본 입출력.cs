05ba91ee4a8f14f7e681a216860997e8b72ff965
강준서 <rkdwnstj0528@gmail.com>
2024-10-14 17:15:12 +0900

    연습

---
+++ 출력문/기본 입출력.cs
@@ -0,0 +1,13 @@
+namespace 연습장
+{
+    class Program
+    {
+        static void Main(string[] args)}
+        {
+             Console.WriteLine("000");
+        //주석
+    }
+        }
+    {
+       
+}
